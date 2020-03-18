e86ca8e1aae825b21925733c4e99618cef83edda
Martin Di Palma <tincho.dipalma@gmail.com>
2020-03-18 18:30:54 -0300

    first approach

---
+++ functions/index.js
@@ -0,0 +1,5 @@
+const functions = require('firebase-functions');
+
+const server = require('./server');
+
+exports.api = functions.https.onRequest(server);
---
+++ functions/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const server = express();
+server.use(cors());
+
+server.use(routes);
+
+module.exports = server;
